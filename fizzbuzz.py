89c47be966999d5fe5799a8562cfa4c27e119f75
Fadi <fadiadrah@yahoo.com>
2025-12-17 18:46:49 -0800

    fizzbuzz commit

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+def fizzBuzz(input):
+    for i in range(1, input + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+if __name__ == "__main__":
+    input = int(input().strip())
+    fizzBuzz(input)
